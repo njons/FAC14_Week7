82d87e685957a3dcd64fdca369e511d2a22c8564
Nathalie Jonsson <jonsson.nathalie@gmail.com>
2018-09-01 09:49:58 +0100

    login and register routes

--- src/handler.js
+++ src/handler.js
@@ -1,5 +1,12 @@
 const fs = require("fs");
 const path = require("path");
+const querystring = require("querystring");
+const jwt = require("jsonwebtoken");
+
+const verifyLogin = require("./queries/login");
+const register = require("./queries/register");
+
+const key = "jaffa";
 
 const homeRoute = (request, response) => {
   fs.readFile(
@@ -37,9 +44,90 @@ const publicRoute = (request, response, url) => {
   });
 };
 
-const loginRoute = (request, response, url) => {};
+const loginRoute = (request, response, url) => {
+  // console.log("this is url", url);
+  let data = "";
+  request.on("data", chunk => {
+    data += chunk;
+  });
+  request.on("end", () => {
+    const username = querystring.parse(data).username;
+    const password = querystring.parse(data).password;
 
-const registerRoute = (request, response, url) => {};
+    if (data) {
+      verifyLogin(username, password, (err, data) => {
+        if (err) {
+          response.writeHead(500, { "content-type": "html/text" });
+          response.end("<h1>error updating the database</h1>");
+        } else if (data) {
+          console.log("bang:", data);
+          const payload = { logged_in: true, username: data };
+          const options = { expiresIn: "30d" };
+          const token = jwt.sign(payload, key, options);
+          response.writeHead(302, {
+            location: "/",
+            "Set-Cookie": `status=true; HttpOnly`
+          });
+          response.end();
+        } else {
+          response.writeHead(302, {
+            location: "/",
+            "Set-Cookie": "status=0; Max-Age=0"
+          });
+          response.end();
+        }
+      });
+    }
+  });
+};
+
+const registerRoute = (request, response, url) => {
+  console.log("this is url", url);
+  let data = "";
+  request.on("data", chunk => {
+    data += chunk;
+  });
+  request.on("end", () => {
+    console.log("(handler) this is data back from register:", data);
+    const username = querystring.parse(data).username;
+    const password = querystring.parse(data).password;
+    const colour = querystring.parse(data).color;
+    if (data) {
+      register(username, password, colour, (err, username) => {
+        if (err) {
+          response.writeHead(500, { "content-type": "html/text" });
+          response.end("<h1>Woops womething went wrong</h1>");
+        } else {
+          console.log("bong:", username);
+          const payload = {
+            logged_in: true,
+            user_name: username
+          };
+          const options = { expiresIn: "30d" };
+          const token = jwt.sign(payload, key, options);
+          response.writeHead(302, {
+            location: "/",
+            "Set-Cookie": `status=${token}; HttpOnly`
+          });
+          response.end();
+        }
+      });
+    }
+  });
+
+  fs.readFile(
+    path.join(__dirname, "..", "public", "register.html"),
+    (err, file) => {
+      if (err) {
+        response.writeHead(500, { "Content-type": "text/html" });
+        response.end("<h1>Woops womething webt wrong</h1>");
+      } else {
+        response.writeHead(200, { "Content-type": "text/html" });
+        response.end(file);
+      }
+    }
+  );
+};
 
 const logoutRoute = (request, response, url) => {};
 
---
+++ src/queries/login.js
@@ -0,0 +1,36 @@
+const dbConnection = require("../db/db_connection");
+const bcryptjs = require("bcryptjs");
+
+const verifyLogin = (username, password, cb) => {
+  console.log(username);
+  // ask db for the hashed password that belongs to the unique username (to match with the input)
+  dbConnection.query(
+    `SELECT password, id FROM users WHERE username = $1;`,
+    [username],
+    (err, dbResult) => {
+      console.log(
+        "(login) this the hashed password (in verifyLogin):",
+        dbResult.rows[0].password
+      );
+      if (err) return cb(err);
+      // use bcrypt to compare the hased password (dbResult) with the input (password)
+      bcryptjs.compare(
+        password,
+        dbResult.rows[0].password,
+        (err, checkedPw) => {
+          if (err) return cb(err);
+          // if there is a match
+          if (checkedPw) {
+            // return the id of the user to the router to be placed in the cookie
+            cb(null, checkedPw);
+          } else {
+            // if the two do not match
+            cb(null, false);
+          }
+        }
+      );
+    }
+  );
+};
+
+module.exports = verifyLogin;
