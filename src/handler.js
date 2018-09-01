d7da08f3076dc489970f817b1901c2140f3ada46
Nathalie Jonsson <jonsson.nathalie@gmail.com>
2018-09-01 14:19:51 +0100

    added register route and welcome page

--- src/handler.js
+++ src/handler.js
@@ -2,9 +2,11 @@ const fs = require("fs");
 const path = require("path");
 const querystring = require("querystring");
 const jwt = require("jsonwebtoken");
+const cookie = require("cookie");
 
 const verifyLogin = require("./queries/login");
 const register = require("./queries/register");
+const getUserInfo = require("./queries/getUserInfo");
 
 const key = "jaffa";
 
@@ -60,13 +62,16 @@ const loginRoute = (request, response, url) => {
           response.writeHead(500, { "content-type": "html/text" });
           response.end("<h1>error updating the database</h1>");
         } else if (data) {
-          console.log("bang:", data);
-          const payload = { logged_in: true, username: data };
+          console.log("(handler) bang:", data);
+          const payload = {
+            logged_in: true,
+            id: data
+          };
           const options = { expiresIn: "30d" };
           const token = jwt.sign(payload, key, options);
           response.writeHead(302, {
-            location: "/",
-            "Set-Cookie": `status=true; HttpOnly`
+            location: "/welcome",
+            "Set-Cookie": `status=${token}; HttpOnly`
           });
           response.end();
         } else {
@@ -82,6 +87,18 @@ const loginRoute = (request, response, url) => {
 };
 
 const registerRoute = (request, response, url) => {
+  fs.readFile(
+    path.join(__dirname, "..", "public", "register.html"),
+    (err, file) => {
+      if (err) {
+        response.writeHead(500, { "Content-type": "text/html" });
+        response.end("<h1>Woops womething webt wrong</h1>");
+      } else {
+        response.writeHead(200, { "Content-type": "text/html" });
+        response.end(file);
+      }
+    }
+  );
   console.log("this is url", url);
   let data = "";
   request.on("data", chunk => {
@@ -93,20 +110,22 @@ const registerRoute = (request, response, url) => {
     const password = querystring.parse(data).password;
     const colour = querystring.parse(data).color;
     if (data) {
-      register(username, password, colour, (err, username) => {
+      register(username, password, colour, (err, userId) => {
+        // console.log("(handler) this is userId:", userId);
         if (err) {
           response.writeHead(500, { "content-type": "html/text" });
           response.end("<h1>Woops womething went wrong</h1>");
         } else {
-          console.log("bong:", username);
+          console.log("(handler) bong:", userId);
           const payload = {
             logged_in: true,
-            user_name: username
+            id: userId
           };
           const options = { expiresIn: "30d" };
           const token = jwt.sign(payload, key, options);
+          console.log("this is token:", token);
           response.writeHead(302, {
-            location: "/",
+            location: "/welcome",
             "Set-Cookie": `status=${token}; HttpOnly`
           });
           response.end();
@@ -114,9 +133,36 @@ const registerRoute = (request, response, url) => {
       });
     }
   });
+};
 
+const welcomeRoute = (request, response, url) => {
+  // check if there is a cookie
+  console.log("(handler) this is the request:", request.headers.cookie);
+  if (request.headers.cookie && request.headers.cookie.includes("status")) {
+    // use the module cookie to parse the information in the cookie
+    const cookies = cookie.parse(request.headers.cookie);
+    console.log("(handler) this is cookie:", cookie);
+    // use the module jsonwebtoken to verify the infomatin in the cookie
+    jwt.verify(cookies.status, key, (err, decoded) => {
+      console.log("(handler) this is decoded:", decoded);
+      console.log("(handler) this is decoded.id is true:", decoded.id);
+      getUserInfo(decoded.id, (err, decoded) => {
+        if (err) console.log(err);
+        console.log("hi ", name);
+        const returnObj = {
+          username: name,
+          user_id: decoded.user_id,
+          logged_in: decoded.logged_in,
+          days: data
+        };
+        console.log("returnObj:", returnObj);
+        // res.writeHead(200, { 'content-type': 'application/json' });
+        // return res.end(JSON.stringify(returnObj));
+      });
+    });
+  }
   fs.readFile(
-    path.join(__dirname, "..", "public", "register.html"),
+    path.join(__dirname, "..", "public", "welcome.html"),
     (err, file) => {
       if (err) {
         response.writeHead(500, { "Content-type": "text/html" });
@@ -136,5 +182,6 @@ module.exports = {
   publicRoute,
   loginRoute,
   registerRoute,
+  welcomeRoute,
   logoutRoute
 };
---
+++ src/queries/getUserInfo.js
@@ -0,0 +1,15 @@
+const dbConnection = require("../db/db_connection");
+
+const getUserInfo = (id, cb) => {
+  console.log("(getUserInfo) this is id:", id);
+  // get the username and colour
+  dbConnection.query(
+    `SELECT username, colour FROM users WHERE id=$1`,
+    [id],
+    (err, dbResults) => {
+      console.log("(getUserInfo) this is db Results:", dbResults);
+    }
+  );
+};
+
+module.exports = getUserInfo;
