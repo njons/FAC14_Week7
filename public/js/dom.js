536bea1b3011c9217c4a53db7e1490d62a38beaf
Nathalie Jonsson <jonsson.nathalie@gmail.com>
2018-11-15 10:34:42 +0100

    validation

--- public/js/dom.js
+++ public/js/dom.js
@@ -23,17 +23,3 @@ function getWelcomeData() {
     }
   });
 }
-
-function xhrRequest(method, url, cb) {
-  const xhr = new XMLHttpRequest();
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState === 4 && xhr.status === 200) {
-      console.log("this is the xhr data:", xhr.responseText);
-      cb(null, JSON.parse(xhr.responseText));
-    } else if (xhr.readyState === 4 && xhr.status !== 200) {
-      cb("error" + xhr.responseType);
-    }
-  };
-  xhr.open(method, url, true);
-  xhr.send();
-}
