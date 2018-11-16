c9039a802d78aede9dabcfaa0854ca2c270fdd7f
Nathalie Jonsson <jonsson.nathalie@gmail.com>
2018-11-16 18:40:23 +0100

    add tests for logout route

--- __test__/route.test.js
+++ __test__/route.test.js
@@ -25,25 +25,18 @@ describe("Test the home GET route", () => {
   });
 });
 
-describe("Test the publicRoute, serving files", () => {
-  test("Returns with status code 200", done => {
+describe("Test the routes, serving files", () => {
+  test("public files (css): status code 200, correct MIME type and serves css", done => {
     supertest(router)
       .get("/public/css/style.css")
       .then(response => {
         expect(response.statusCode).toBe(200);
-        done();
-      });
-  });
-  test(".css files returns css", done => {
-    supertest(router)
-      .get("/public/css/style.css")
-      .then(response => {
         expect(response.res.statusMessage).toBe("Content-type: text/css");
         expect(response.res.text.charAt(0)).toBe("*");
         done();
       });
   });
-  test(".js files returns javascript", done => {
+  test("public files (js): status code 200, correct MIME type and serves javascript", done => {
     supertest(router)
       .get("/public/js/dom.js")
       .then(response => {
@@ -55,7 +48,7 @@ describe("Test the publicRoute, serving files", () => {
         done();
       });
   });
-  test("font file returns .ttf", done => {
+  test("public files (font): status code 200 and correct MIME type", done => {
     supertest(router)
       .get("/public/css/font/InputMono-Bold.ttf")
       .then(response => {
@@ -66,19 +59,72 @@ describe("Test the publicRoute, serving files", () => {
         done();
       });
   });
-});
-
-describe("Test the login route", () => {
-  test("the /login returns a status code of 200 ", done => {
+  test("register page: status code of 200, correct MIME type and serves html", done => {
+    supertest(router)
+      .get("/register")
+      .then(response => {
+        expect(response.statusCode).toBe(200);
+        expect(response.header["content-type"]).toBe("text/html");
+        expect(response.res.text.charAt(0)).toBe("<");
+        done();
+      });
+  });
+  test("login page: status code of 200, correct MIME type and serves html", done => {
     supertest(router)
       .get("/login")
       .then(response => {
         expect(response.statusCode).toBe(200);
+        expect(response.header["content-type"]).toBe("text/html");
+        expect(response.res.text.charAt(0)).toBe("<");
+        done();
+      });
+  });
+  test("welcome page: status code of 200, correct MIME type and serves html", done => {
+    supertest(router)
+      .get("/welcome")
+      .then(response => {
+        expect(response.statusCode).toBe(200);
+        expect(response.header["content-type"]).toBe("text/html");
+        expect(response.res.text.charAt(0)).toBe("<");
         done();
       });
   });
 });
-//
+
+describe("Test rerouting pages", () => {
+  test("user-data route: status code of 302 and reroutes to the welcome page", done => {
+    supertest(router)
+      .get("/user-data")
+      .then(response => {
+        console.log(response.headers.location);
+        expect(response.statusCode).toBe(302);
+        done();
+      });
+  });
+  test("logout route: status code of 302 and reroutes to the home page", done => {
+    supertest(router)
+      .get("/logout")
+      .then(response => {
+        expect(response.statusCode).toBe(302);
+        expect(response.headers.location).toBe("/");
+        done();
+      });
+  });
+  test("logout route: contains instructions for cookie that destroys the cookie", done => {
+    supertest(router)
+      .get("/logout")
+      .then(response => {
+        expect(typeof response.headers["set-cookie"]).toBe("object");
+        expect(response.headers["set-cookie"][0].slice(10, 19)).toBe(
+          "Max-Age=0"
+        );
+        done();
+      });
+  });
+});
+
+// verify login (gets stuck in validation function - because details are undefined?)
+
 // describe("Test the verify login route", () => {
 //   test("the /verify-login returns a status code of 200 ", done => {
 //     supertest(router)
@@ -91,16 +137,7 @@ describe("Test the login route", () => {
 //   });
 // });
 
-describe("Test the register route", () => {
-  test("the /register returns a status code of 200 ", done => {
-    supertest(router)
-      .post("/register")
-      .then(response => {
-        expect(response.statusCode).toBe(200);
-        done();
-      });
-  });
-});
+// save registry (gets stuck in validation function - because details are undefined?)
 
 // describe("Test saving the registry route", () => {
 //   test("the /save-registry returns a status code of 200 ", done => {
@@ -113,40 +150,6 @@ describe("Test the register route", () => {
 //       });
 //   });
 // });
-
-describe("Test the welcome route", () => {
-  test("the /welcome returns a status code of 200 ", done => {
-    supertest(router)
-      .get("/welcome")
-      .then(response => {
-        expect(response.statusCode).toBe(200);
-        done();
-      });
-  });
-});
-
-describe("Test the welcome data route", () => {
-  test("the /user-data returns a status code of 302 ", done => {
-    supertest(router)
-      .get("/user-data")
-      .then(response => {
-        expect(response.statusCode).toBe(302);
-        done();
-      });
-  });
-});
-
-describe("Test the logout data route", () => {
-  test("the /logout returns a status code of 302 ", done => {
-    supertest(router)
-      .get("/logout")
-      .then(response => {
-        console.log(response);
-        expect(response.statusCode).toBe(302);
-        done();
-      });
-  });
-});
 
 // // PAGE: this is where we are serving the login files
 // homeRoute(request, response, url);
